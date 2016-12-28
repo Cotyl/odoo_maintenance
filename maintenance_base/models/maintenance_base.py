9b59e03d3b44e5bccc7dc998f346b8bbdb7a2a0b
clementthomas <clement.thomas@gmail.com>
2016-12-28 13:21:26 +0100

    State management and copy correction

--- maintenance_base/models/maintenance_base.py
+++ maintenance_base/models/maintenance_base.py
@@ -7,7 +7,25 @@ Created on 25 oct. 2016
 from odoo import models, fields, api
 from datetime import datetime, timedelta, time
 from odoo.tools import DEFAULT_SERVER_DATETIME_FORMAT as DTF
+from odoo.exceptions import UserError
 import pytz
+
+class sale_order_line(models.Model):
+    _inherit = 'sale.order.line'
+    
+    @api.multi
+    def unlink(self):
+        if self._context.get('force',False):
+            return super(sale_order_line,self).unlink()
+
+        sale_line_intervention_sale_done = self.filtered(lambda x: x.order_id.is_maintenance_intervention and x.state in ('sale', 'done'))
+        for sale_line in sale_line_intervention_sale_done:
+            sale_line.state = 'draft'
+        sale_line_intervention_sale_done.with_context(force=True).unlink()
+        
+        sale_line_others = self.filtered(lambda x: not x.order_id.is_maintenance_intervention or x.state not in ('sale', 'done'))
+        sale_line_others.with_context(force=True).unlink()
+        return 0
 
 class sale_order(models.Model):
     _inherit = 'sale.order'
@@ -20,6 +38,17 @@ class sale_order(models.Model):
             args.append(('is_maintenance_intervention','=',False))
             
         return super(sale_order, self).search(args, offset=offset, limit=limit, order=order, count=count)
+    
+    @api.model
+    @api.returns('self', lambda value:value.id)
+    def create(self, vals):
+        if self._context.get('copy_intervention',False):
+            # when we copy sale order from intervention copy, 
+            # order lines are automatically created from intervention lines.
+            if 'order_line' in vals:
+                del vals['order_line'] 
+        res = super(sale_order,self).create(vals)
+        return res
 
 class maintenance_installation(models.Model):
     _name = 'maintenance.installation'
@@ -28,7 +57,7 @@ class maintenance_installation(models.Model):
     name = fields.Char("Identification", size=255, help="A name for your installation understandable.")
     partner_id = fields.Many2one('res.partner', string='Customer',help="The partner linked to this maintenance installation", required=True)
     address_id = fields.Many2one('res.partner', string='Address', help="The address where the installation is located")
-    contact_address_id = fields.Many2one('res.partner', string='Contact address', domain=[('type','=','contact')],help="The contact person for this installation")
+    contact_address_id = fields.Many2one('res.partner', string='Contact', domain=[('type','=','contact')],help="The contact person for this installation")
     elements = fields.One2many('maintenance.element', 'installation_id', "Elements",help="The elements contained in this installation")
     interventions = fields.One2many('maintenance.intervention', 'installation_id', "Interventions", help="The interventions linked to this installation")
     note=fields.Text('Notes')
@@ -57,16 +86,16 @@ class maintenance_installation(models.Model):
 class maintenance_element(models.Model):
     _name = 'maintenance.element'
     
-    installation_id = fields.Many2one('maintenance.installation', 'Installation', index=True)
-    code = fields.Char("Reference", required=True, size=255,default=lambda obj: obj.env['ir.sequence'].get('maintenance.element'))
+    installation_id = fields.Many2one('maintenance.installation', 'Installation', index=True, required=True)
     name = fields.Char("Name", size=255) 
-    product_id = fields.Many2one('product.product', 'Product')
     serial_number = fields.Char(string="Serial number")
     description = fields.Text("Description")
     installation_date = fields.Date("Installation date")
     warranty_date = fields.Date("Warranty date")
     location = fields.Char("Location", size=255)
-    state = fields.Selection([('active', 'Active'), ('inactive','Inactive')], string="State", readonly=True,help="Inactive older installations.")
+    state = fields.Selection([('active', 'Active'), ('inactive','Inactive')], string="State", help="Inactive older installations.", default="active")
+    
+   
     
 class maintenance_intervention_type(models.Model):
     _name="maintenance.intervention.type"
@@ -83,6 +112,43 @@ class maintenance_intervention(models.Model):
     
     _inherits = {'sale.order': 'sale_id'}
     
+    _order = 'id desc'
+    
+    @api.multi
+    def action_draft(self):
+        self.sale_id.action_draft()
+        self.state = 'draft'
+    
+    @api.multi
+    def action_cancel(self):
+        self.sale_id.action_cancel()
+        
+        #Cancel all linked invoices
+        invoices = set()
+        for sale_line in self.sale_id.order_line:
+            for invoice_line in sale_line.invoice_lines:
+                invoices.add(invoice_line.invoice_id) 
+        for invoice in invoices:
+            invoice.action_cancel()
+            
+        self.state = 'cancel'
+    
+    @api.multi
+    def action_confirm(self):
+        self.sale_id.action_confirm()
+        self.state = 'confirmed'
+        
+    @api.multi
+    def action_done(self):
+        #create invoice if lines to invoice exists
+        if self.sale_id.order_line.filtered(lambda l:l.qty_to_invoice > 0):
+            self.sale_id.action_invoice_create(final=True)
+        self.state = 'done'
+    
+    @api.multi
+    def print_intervention(self):
+        return self.env['report'].get_action(self, 'maintenance_base.report_maintenance_intervention')
+    
     @api.onchange('installation_id')
     def sync_partner_id(self):
         self.partner_id = self.installation_id.partner_id
@@ -93,12 +159,33 @@ class maintenance_intervention(models.Model):
             intervention.sale_id.unlink()
         return super(maintenance_intervention,self).unlink()
     
+    @api.multi
+    def sync_state(self):
+        for intervention in self:
+            if intervention.state in ('confirmed','planned'):
+                state = 'confirmed'
+                for task in intervention.intervention_tasks:
+                    if task.date_start:
+                        state = 'planned'
+                        break
+                intervention.with_context(no_sync=True).state = state
+                    
+    
     @api.model
     @api.returns('self', lambda value:value.id)
     def create(self, vals):
         vals['partner_id'] = self.env['maintenance.installation'].browse(vals['installation_id']).partner_id.id
-        vals['is_maintenance_intervention'] = True 
+        vals['is_maintenance_intervention'] = True
         return super(maintenance_intervention,self).create(vals)
+        
+        
+    
+    @api.multi
+    def write(self, vals):
+        res = super(maintenance_intervention, self).write(vals)
+        if not self._context.get('no_sync',False): #prevent loop
+            self.sync_state()
+        return res
     
     @api.multi
     def name_get(self):
@@ -110,13 +197,20 @@ class maintenance_intervention(models.Model):
             res.append((intervention.id, name))
         return res
     
+    @api.multi
+    @api.returns('self', lambda value:value.id)
+    def copy(self, default=None):
+        if not 'code' in default:
+            default['code'] = self.env['ir.sequence'].get('maintenance.intervention')
+        return super(maintenance_intervention,self.with_context(copy_intervention=True)).copy(default)
+    
     code = fields.Char("Reference", size=255, required=True,default=lambda obj: obj.env['ir.sequence'].get('maintenance.intervention'))
     name = fields.Text("Description", index=True)
     installation_id = fields.Many2one('maintenance.installation', string='Installation', required=True) 
     intervention_type_id = fields.Many2one('maintenance.intervention.type', string='Type', required=True)
     state = fields.Selection([('draft','Draft'), ('confirmed', 'Confirmed'),('planned', 'Planned'), ('done', 'Done'), ('cancel','Cancel')], string="Intervention State", readonly=True,default='draft')
-    intervention_tasks = fields.One2many('maintenance.intervention.task','intervention_id', 'Tasks')
-    intervention_lines = fields.One2many('maintenance.intervention.line','intervention_id', 'Products')
+    intervention_tasks = fields.One2many('maintenance.intervention.task','intervention_id', 'Tasks', copy=True)
+    intervention_lines = fields.One2many('maintenance.intervention.line','intervention_id', 'Products', copy=True)
     date_scheduled=fields.Date('Scheduled date')
     time_planned = fields.Float('Time planned', help='Time initially planned to do intervention.')
     internal_comment = fields.Text("Internal comment")
